709c3255a3fd369f41a6ca24f307d2ab6e10bb74
usongon <zdhunter@foxmail.com>
2023-11-06 22:26:28 +0800

    init

---
+++ src/main/java/com/usongon/usongtool/UsongToolApplication.java
@@ -0,0 +1,13 @@
+package com.usongon.usongtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsongToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsongToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/usongon/usongtool/UsongToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usongon.usongtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsongToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
